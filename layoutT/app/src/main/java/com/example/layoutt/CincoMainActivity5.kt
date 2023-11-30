0eec2b57b3756bd31e6af8d551be7974f53a0715
Techie1 <garcia.da.heydi@gmail.com>
2023-11-30 09:20:21 +0100

    gergerg

---
+++ layoutT/app/src/main/java/com/example/layoutt/CincoMainActivity5.kt
@@ -0,0 +1,11 @@
+package com.example.layoutt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CincoMainActivity5 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cinco_main5)
+    }
+}
---
+++ layoutT/app/src/main/java/com/example/layoutt/tresMainActivity2.kt
@@ -0,0 +1,11 @@
+package com.example.layoutt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class tresMainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tres_main2)
+    }
+}
